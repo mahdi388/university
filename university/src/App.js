9078738e4152ef20bea92698ae0d63fd53588915
mahdi388 <ahangarym34@yahoo.com>
2023-09-05 21:09:08 +0330

    add navbar

--- university/src/App.js
+++ university/src/App.js
@@ -1,5 +1,44 @@
+import './styles/main.scss'
+import {Routes,Route,useNavigate} from 'react-router-dom'
+
 function App() {
-  return <>app</>;
+  const navigate=useNavigate()
+  return <>
+    <nav className='nav-bar'>
+      <ul>
+        <li onClick={() => navigate('/')}>
+          <div></div>
+          <span>دانشجویان</span>
+          <div></div>
+        </li>
+        <li onClick={() => navigate('/movies')}>
+          <div></div>
+          <span>دروس</span>
+          <div></div>
+        </li>
+        <li onClick={() => navigate('/about-us')}>
+          <div></div>
+          <span>اساتید</span>
+          <div></div>
+        </li>
+        <li onClick={() => navigate('/about-us')}>
+          <div></div>
+          <span>واحدها</span>
+          <div></div>
+        </li>
+      </ul>
+      <div className="search">
+        <input type="text" placeholder='جست‌ و جو'/>
+        <div>
+          <i className="fa fa-search"></i>
+        </div>
+      </div>
+    </nav>
+    <main>
+      <Routes>
+      </Routes>
+    </main>
+  </>;
 }
 
 export default App;
